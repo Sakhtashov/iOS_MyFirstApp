2a9063631a8b2bc6adcec1510f0f9e310bf487ee
Yerkhat Sakhtashov <200103307@stu.sdu.edu.kz>
2022-10-29 00:36:03 +0600

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Ерхат Сахташов on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
